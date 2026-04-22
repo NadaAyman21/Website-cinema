b0977c3fec6782702566505f317314fc33f91a15
razan <razan2400816@miuegypt.edu.eg>
2026-04-22 17:36:49 +0200

    adding footer css

--- login.js
+++ login.js
@@ -8,41 +8,6 @@ function togglePassword() {
         password.type = "password";
     }
 }
-
-function validateEmail(){
-    let email = document.getElementById("email").value.trim();
-    let emailError = document.getElementById("emailError");
-
-    emailError.innerText = "";
-
-    let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-
-    if(email === "" || !email.match(pattern)){
-        emailError.innerText = "Enter a valid email";
-        return false;
-    }
-
-    return true;
-}
-
-function validatePassword(){
-    let password = document.getElementById("password").value.trim();
-    let passwordError = document.getElementById("passwordError");
-
-    passwordError.innerText = "";
-
-    let passwordPattern = /^(?=.*[A-Z])(?=.*[0-9]).{6,}$/;
-
-    if(!password.match(passwordPattern)){
-        passwordError.innerText =
-            "Password must contain 1 uppercase, 1 number, min 6 chars";
-        return false;
-    }
-
-    return true;
-}
-
-  
 const form = document.getElementById("loginForm");
 
 form.addEventListener("submit", function(e){
