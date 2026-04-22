a59ef598dba568c28b3099a5ff70df95d4245451
razan <razan2400816@miuegypt.edu.eg>
2026-04-22 21:00:22 +0200

    adding function gotologin

--- nav.js
+++ nav.js
@@ -2,3 +2,7 @@ function toggleMenu() {
     let nav = document.getElementById("navLinks");
     nav.classList.toggle("active");
 }
+
+function goToLogin(){
+    window.location.href = "login.html";
+}
