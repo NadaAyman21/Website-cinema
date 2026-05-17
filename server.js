8791b497ce4d241fb9bb355e1e9b6c6f44326d0d
habiba <habiba2402874@miuegypt.edu.eg>
2026-05-17 21:28:19 +0300

    allow server to read from data

--- server.js
+++ server.js
@@ -12,7 +12,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
+app.use(express.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'cinema_secret_key',
+  resave: false,
+  saveUninitialized: false
+}));
 
 
 app.use(express.static(path.join(__dirname, 'public')));
