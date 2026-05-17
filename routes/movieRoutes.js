a8f86625fe6d06ce1038ece115579ea623fcddc5
nada <nada2407439@miuegypt.edu.eg>
2026-05-17 21:37:25 +0300

    router.get function

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -2,11 +2,9 @@ const express = require('express');
 const router = express.Router();
 const Movie = require('../models/Movie'); 
 
-// POST: Create a new movie
 router.post('/add', async (req, res) => {
   
    try {
-        // Map frontend client array split if needed
         const movieData = {
             ...req.body,
             cast: typeof req.body.cast === 'string' ? req.body.cast.split(',').map(c => c.trim()) : req.body.cast
@@ -19,6 +17,15 @@ router.post('/add', async (req, res) => {
     }
 });
 
+router.get('/', async (req, res) => {
+        try {
+        const movies = await Movie.find().sort({ createdAt: -1 });
+        res.json(movies);
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message });
+    }
+
+});
 
 
 
