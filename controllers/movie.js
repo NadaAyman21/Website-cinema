68de0e6ba96e0a37c328b08a2a0c81318c3dbd60
nada <nada2407439@miuegypt.edu.eg>
2026-05-23 20:12:32 +0300

    getMoviebyId function

--- controllers/movie.js
+++ controllers/movie.js
@@ -9,3 +9,15 @@ exports.getAllMovies = async (req, res) => {
         res.status(500).json({ success: false, message: err.message });
     }
 };
+
+exports.getMovieById = async (req, res) => {
+    try {
+        const movie = await Movie.findById(req.params.id);
+        if (!movie) {
+            return res.status(404).json({ success: false, message: "Movie not found" });
+        }
+        res.json(movie);
+    } catch (err) {
+        res.status(500).json({ success: false, message: err.message });
+    }
+};
