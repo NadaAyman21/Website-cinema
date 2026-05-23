5497b0e4ddeee2c8f39b3b05a8e951c5d492c175
nada <nada2407439@miuegypt.edu.eg>
2026-05-24 02:55:37 +0300

    aadd new file

---
+++ public/js/rev.js
@@ -0,0 +1,9 @@
+ const categorySelect = document.getElementById('category');
+        const movieGroup = document.getElementById('movie-select-group');
+        const experienceGroup = document.getElementById('experience-select-group');
+        const foodGroup = document.getElementById('food-select-group');
+
+        const movieSelect = document.getElementById('movie-item');
+        const experienceSelect = document.getElementById('experience-item');
+        const foodSelect = document.getElementById('food-item');
+        const hiddenItemInput = document.getElementById('item');
