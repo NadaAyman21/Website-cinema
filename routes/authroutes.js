6466599d8d8955b8c3e96c6cc57647f397a76879
habiba <habiba2402874@miuegypt.edu.eg>
2026-05-23 16:10:55 +0300

    added update profile

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -4,5 +4,6 @@ const authController = require('../controllers/authController');
 
 router.post('/signup', authController.signupPost);
 router.post('/login', authController.loginPost);
+router.post('/updateProfile', authController.updateProfile);
 
 module.exports = router;
