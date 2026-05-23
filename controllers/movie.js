ac0b9c52a6e7cf1fb30158b1536f439ef51fe766
nada <nada2407439@miuegypt.edu.eg>
2026-05-23 20:13:00 +0300

    addMovie function

--- controllers/movie.js
+++ controllers/movie.js
@@ -21,3 +21,17 @@ exports.getMovieById = async (req, res) => {
         res.status(500).json({ success: false, message: err.message });
     }
 };
+
+exports.addMovie = async (req, res) => {
+    try {
+        const movieData = {
+            ...req.body,
+            cast: typeof req.body.cast === 'string' ? req.body.cast.split(',').map(c => c.trim()) : req.body.cast
+        };
+        const newMovie = new Movie(movieData);
+        const savedMovie = await newMovie.save();
+        res.status(201).json(savedMovie);
+    } catch (err) {
+        res.status(400).json({ success: false, message: err.message });
+    }
+};
