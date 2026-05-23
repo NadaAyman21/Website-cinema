7b4dad095f2ea8f8ee65f48b819184be2d84d9b4
habiba <habiba2402874@miuegypt.edu.eg>
2026-05-23 16:30:13 +0300

    added password route

--- routes/authroutes.js
+++ routes/authroutes.js
@@ -5,5 +5,6 @@ const authController = require('../controllers/authController');
 router.post('/signup', authController.signupPost);
 router.post('/login', authController.loginPost);
 router.post('/updateProfile', authController.updateProfile);
+router.post('/changePassword', authController.changePassword);
 
 module.exports = router;
