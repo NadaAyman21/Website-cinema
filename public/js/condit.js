1e3b35921badecd24d52d138e1e19e0a2d8bfd79
habiba <habiba2402874@miuegypt.edu.eg>
2026-05-25 15:56:38 +0300

    changing contitions theme

--- public/js/condit.js
+++ public/js/condit.js
@@ -3,11 +3,15 @@ window.onload = function() {
     const urlParams = new URLSearchParams(window.location.search);
     currentType = urlParams.get('type'); // "premiere" or "standard"
 
+    const modalBox = document.getElementById("modalBox"); // ADD THIS
+
     if (currentType === 'premiere') {
         document.getElementById("premiereRule").style.display = "block";
+        modalBox.classList.add("gold-theme"); // ADD THIS
+    } else {
+        modalBox.classList.add("blue-theme"); // ADD THIS
     }
 };
-
 function agree() {
     if (currentType === "premiere") {
         window.location.href = "/vipSeats";   // Go to VIP seats
